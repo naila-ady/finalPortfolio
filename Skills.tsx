bed7983544692c5827b33f9458f9d6c38a7ca1a3
naila-ady <164941279+naila-ady@users.noreply.github.com>
2024-11-10 22:08:25 +0500

    Add files via upload

---
+++ Skills.tsx
@@ -0,0 +1,146 @@
+const Skills=() =>{
+    return(
+    <div className="bg-[#e1ecf3]" id="Skills">
+<section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="text-center mb-20">
+      <h1 className="sm:text-3xl  text-6xl font-medium text-center title-font text-[#003152] mb-4">
+        My Skills
+      </h1>
+      <p className="text-xl leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-[#003152] ">
+      Some of the skills I have developed over time as a Front-end web developer are:
+      </p>
+    </div>
+    <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+      <div className="p-2 sm:w-1/2 w-full">
+        <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+          <svg
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={3}
+            className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4"
+            viewBox="0 0 24 24"
+          >
+            <path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
+            <path d="M22 4L12 14.01l-3-3" />
+          </svg>
+          <span className="title-font font-semibold text-[#003152]">
+          HTML5 &
+              CSS3
+          </span>
+        </div>
+      </div>
+      <div className="p-2 sm:w-1/2 w-full">
+        <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+          <svg
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={3}
+            className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4"
+            viewBox="0 0 24 24"
+          >
+            <path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
+            <path d="M22 4L12 14.01l-3-3" />
+          </svg>
+          <span className="title-font font-medium text-[#003152]">
+          JavaScript
+          TypeScript
+          </span>
+        </div>
+      </div>
+      <div className="p-2 sm:w-1/2 w-full">
+        <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+          <svg
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={3}
+            className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4"
+            viewBox="0 0 24 24"
+          >
+            <path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
+            <path d="M22 4L12 14.01l-3-3" />
+          </svg>
+          <span className="title-font font-medium text-[#003152]">
+            Next.js
+          React</span>
+        </div>
+      </div>
+      <div className="p-2 sm:w-1/2 w-full">
+        <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+          <svg
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={3}
+            className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4"
+            viewBox="0 0 24 24"
+          >
+            <path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
+            <path d="M22 4L12 14.01l-3-3" />
+          </svg>
+          <span className="title-font font-medium text-[#003152]">
+          UI And UX
+          </span>
+        </div>
+      </div>
+      <div className="p-2 sm:w-1/2 w-full">
+        <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+          <svg
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={3}
+            className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4"
+            viewBox="0 0 24 24"
+          >
+            <path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
+            <path d="M22 4L12 14.01l-3-3" />
+          </svg>
+          <span className="title-font font-medium text-[#003152]">
+            Figma</span>
+        </div>
+      </div>
+      <div className="p-2 sm:w-1/2 w-full">
+        <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+          <svg
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={3}
+            className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4"
+            viewBox="0 0 24 24"
+          >
+            <path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
+            <path d="M22 4L12 14.01l-3-3" />
+          </svg>
+          <span className="title-font font-medium text-[#003152]">
+            Git &
+          GitHub</span>
+        </div>
+      </div>
+    </div>
+    <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+      View About
+    </button>
+  </div>
+</section>
+
+
+
+
+
+</div>
+      
+    ) 
+}
+export default Skills
+
---
+++ page.tsx
@@ -0,0 +1,23 @@
+import Hero from "@/app/component/Hero";
+import Contact from "@/app/component/Contact";
+ import Projects from "@/app/component/Projects";
+ import About from "@/app/component/About";
+ import Skills from "@/app/component/Skills";
+
+
+
+
+export default function Home() {
+  return (
+    <div>
+     
+      <Hero/>
+      <About/>
+      <Projects/>
+      <Skills/>
+      <Contact/>
+  </div>
+   
+   
+  );
+}
